8498ada08648c7f2de766c7a47fb0b18e668bb10
Al <awessonn@gmail.com>
2023-06-06 12:04:16 -0700

    added User model to the model folder.

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User');
+const Thought = require('./Thoughts');
+
+// exporting User and thoughts to be used in other areas of the app.
+module.exports = {User, Thought};
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const db = requier('./config/connection');
+const routes = require('./routes');
+
+// set environment variables
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// use middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//use rorutes
+app.use(routes);
+
+
+// connect to mongodb/start server.
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on poty ${PORT}`);
+    });
+});
